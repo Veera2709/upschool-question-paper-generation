badbaec1c1c1d3b68d16c5f9fe9c1f533fde3aea
Sunil225 <sunil.kakarla@rhibhus.com>
2024-07-11 13:39:11 +0530

    added export env in ci-cd.yml for dev

--- .dev-gitlab-deploy.sh
+++ .dev-gitlab-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -f
-server_name=$DEV_SERVER
+server_name=${DEV_SERVER}
 echo "Server name: $server_name"
 ssh -o StrictHostKeyChecking=no ec2-user@$server_name "cd /upschool/questionPaper/dev/upschool-question-paper-generation && sudo git reset --hard && sudo git pull https://gitlab.com/Veera27/upschool-question-paper-generation.git --set-upstream dev && sudo npm install && sudo node node_modules/puppeteer-core/install.js && sudo pm2 restart 0"
