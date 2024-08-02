162d1e098c6ae15fa5ddd82822bbf6d3ef0a5933
Sunil225 <sunil.kakarla@rhibhus.com>
2024-08-02 18:47:14 +0530

    added github http

--- .dev-gitlab-deploy.sh
+++ .dev-gitlab-deploy.sh
@@ -3,4 +3,4 @@
 set -f
 server_name=$DEV_SERVER
 echo "Server name: $server_name"
-ssh -o StrictHostKeyChecking=no ec2-user@$server_name "cd /upschool/questionPaper/dev/upschool-question-paper-generation && sudo git reset --hard && sudo git pull https://github.com/Veera2709/upschool-question-paper-generation.git --set-upstream-to=origin/dev && sudo npm install && sudo node node_modules/puppeteer-core/install.js && sudo pm2 restart 0"
+ssh -o StrictHostKeyChecking=no ec2-user@$server_name "cd /upschool/questionPaper/dev/upschool-question-paper-generation && sudo git reset --hard && sudo git pull https://github.com/Veera2709/upschool-question-paper-generation.git --set-upstream origin/dev && sudo npm install && sudo node node_modules/puppeteer-core/install.js && sudo pm2 restart 0"
